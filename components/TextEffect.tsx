7ff1d79589dc89176387cda5ea0446f4e4d6d486
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-12 09:47:02 -0800

    Added text effect for hero section

---
+++ components/TextEffect.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const TextEffect = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        "Programmer",
+        1500, // wait 1s before replacing "Mice" with "Hamsters"
+        "Web Developer",
+        1500,
+        "Full-Stack Engineer",
+        1500,
+        "Creative Designer",
+        1500,
+      ]}
+      speed={50}
+      className="text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase"
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TextEffect;
