cf1dc18219d6526110cfebcd059f613bb7ec5527
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-12 09:46:05 -0800

    Added particle effect as background for the app

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 
-const Nav = () => {
+interface Props {
+  openNav: () => void;
+}
+
+const Nav = ({ openNav }: Props) => {
   return (
-    <div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+    <div className="w-[100%] sticky z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
       <div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
         <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
           WEB
@@ -14,6 +19,9 @@ const Nav = () => {
         <div className="nav-link">PROJECTS</div>
         <div className="nav-link">BLOG</div>
         <div className="nav-link">CONTACT</div>
+        <div onClick={openNav}>
+          <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-yellow-300" />
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
+import MobileNav from "@/components/MobileNav";
 import Nav from "@/components/Nav";
-import React from "react";
+import React, { useState } from "react";
+import Hero from "@/components/Hero";
 
 const HomePage = () => {
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
   return (
     <div className="overflow-x-hidden">
       <div>
-        <Nav />
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+
+        {/* Hero Section */}
+        <Hero />
       </div>
     </div>
   );
