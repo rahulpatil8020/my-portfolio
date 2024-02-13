1bde8b5617aca2767c8583926a81a6efde5adc0b
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-13 08:32:30 -0800

    necessary changes in next configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable static HTML export
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
