4bf03bbb46f24bfc38fdbccdc06fb93c78b35d44
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-12 16:49:07 -0800

    Blog page ui created

---
+++ components/Blog.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import Image from "next/image";
+import {
+  UserCircleIcon,
+  ChatBubbleLeftRightIcon,
+} from "@heroicons/react/20/solid";
+const Blog = () => {
+  return (
+    <div className="pt-[4rem] md:pt-[8rem] pb-[4rem] bg-[#02050a]">
+      <h1 className="heading">
+        My <span className="text-yellow-400">BLOG</span>
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-[4rem] gap-[3rem] w-[80%] mx-auto">
+        <div>
+          <div className="w-[100%] relative h-[400px]">
+            <Image
+              src="/images/blog3.jpg"
+              alt="blog"
+              layout="fill"
+              className="object-cover"
+            />
+          </div>
+          <div className="text-center mx-auto w-[90%] bg-[#09101a] relative p-[2rem] mt-[-1rem] ">
+            <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
+              December 10, 2023
+            </div>
+            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
+              <div className="flex items-center space-x-3">
+                <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5] " />
+                <p className="text-white">By Rahul</p>
+              </div>
+              <div className="flex items-center space-x-3">
+                <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
+                <p className="text-white">Comments(12)</p>
+              </div>
+            </div>
+            <p className="mt-[1rem] text-white text-[18px] text-semibold">
+              Why you should start using Next.js
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="w-[100%] relative h-[400px]">
+            <Image
+              src="/images/blog3.jpg"
+              alt="blog"
+              layout="fill"
+              className="object-cover"
+            />
+          </div>
+          <div className="text-center mx-auto w-[90%] bg-[#09101a] relative p-[2rem] mt-[-1rem] ">
+            <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
+              December 10, 2023
+            </div>
+            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
+              <div className="flex items-center space-x-3">
+                <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5] " />
+                <p className="text-white">By Rahul</p>
+              </div>
+              <div className="flex items-center space-x-3">
+                <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
+                <p className="text-white">Comments(12)</p>
+              </div>
+            </div>
+            <p className="mt-[1rem] text-white text-[18px] text-semibold">
+              Why you should start using Next.js
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="w-[100%] relative h-[400px]">
+            <Image
+              src="/images/blog3.jpg"
+              alt="blog"
+              layout="fill"
+              className="object-cover"
+            />
+          </div>
+          <div className="text-center mx-auto w-[90%] bg-[#09101a] relative p-[2rem] mt-[-1rem] ">
+            <div className="w-fit px-[2rem] py-[1rem] bg-[#55e6a5] relative mt-[-3rem] text-black font-semibold text-[17px] mx-auto">
+              December 10, 2023
+            </div>
+            <div className="flex flex-col md:flex-row items-center mt-[1rem] space-x-4">
+              <div className="flex items-center space-x-3">
+                <UserCircleIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5] " />
+                <p className="text-white">By Rahul</p>
+              </div>
+              <div className="flex items-center space-x-3">
+                <ChatBubbleLeftRightIcon className="w-[1.5rem] mx-auto h-[1.5rem] text-[#55e6a5]" />
+                <p className="text-white">Comments(12)</p>
+              </div>
+            </div>
+            <p className="mt-[1rem] text-white text-[18px] text-semibold">
+              Why you should start using Next.js
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
