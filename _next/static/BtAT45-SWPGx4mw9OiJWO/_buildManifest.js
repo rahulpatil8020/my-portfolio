723571fd3ede72666b00da3fae725f5bfe9988d3
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-13 08:55:16 -0800

    Deploy Next.js app to Github pages

---
+++ _next/static/BtAT45-SWPGx4mw9OiJWO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/f76788cadc76491e.css","static/chunks/150-7cc305d6407cdf55.js","static/chunks/pages/index-3e7d0c38a98dfa39.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
