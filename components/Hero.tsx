c4763ee72f4b3ea92c392923c45bd8c8822da200
rahulpatil8020 <rahuldpatil20@gmail.com>
2024-02-14 17:47:12 -0800

    replaced ' with &apos;

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
         <div>
           <h1 className="text-[35px] md:text-[50px] text-white font-bold">
-            HI, I'M <span className="text-yellow-400">RAHUL!!</span>
+            HI, I&apos;M <span className="text-yellow-400">RAHUL!!</span>
           </h1>
           <TextEffect />
           <p className="mt-[2rem] text-[18px] text-[#ffffff92]">
